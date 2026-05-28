db2dd0ecdf265b7c4cf2da37f6249c229eca8cd3
msand2808 <misael.sandoval2808@gmail.com>
2026-05-28 16:43:16 -0600

    docs: created basic backend structure and user entity added

---
+++ backend/tracker/src/main/java/com/moneytracker/trackerm/entity/User.java
@@ -0,0 +1,29 @@
+package com.moneytracker.trackerm.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    String username;
+
+    String email;
+
+    LocalDate creationDate;
+
+}
